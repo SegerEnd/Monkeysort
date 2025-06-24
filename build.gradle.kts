57b7a328976546c43ff14433f7436ace10be489f
SegerEnd <gj.endendijk@gmail.com>
2025-06-24 11:25:17 +0200

    Add JavaFX plugin and dependencies to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.20"
+    id("org.openjfx.javafxplugin") version "0.0.13"
     application
 }
 
@@ -14,13 +15,30 @@ repositories {
     mavenCentral()
 }
 
+val javafxVersion = "21"
+val osName = System.getProperty("os.name").lowercase()
+val javafxplatform = when {
+    osName.contains("mac") -> "mac"
+    osName.contains("win") -> ""
+    else -> "linux"
+}
+
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.openjfx:javafx-controls:$javafxVersion:$javafxplatform")
+    implementation("org.openjfx:javafx-fxml:$javafxVersion:$javafxplatform")
+    implementation("org.openjfx:javafx-media:$javafxVersion:$javafxplatform")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+javafx {
+    version = javafxVersion
+    modules = listOf("javafx.controls", "javafx.fxml")
 }
